6abea557a3bfeeb6f9d6d56c0792483ea11ead77
Michał Jankowski <215744@edu.p.lodz.pl>
2021-03-09 13:08:01 +0100

    Innitial commit

---
+++ app/src/main/java/com/example/kalkulator/MenuActivity.java
@@ -0,0 +1,28 @@
+package com.example.kalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void simpleCalcClick(View view) {
+
+    }
+
+    public void advancedCalcClick(View view) {
+    }
+
+    public void aboutCalcClick(View view) {
+    }
+
+    public void exitClick(View view) {
+    }
+}
